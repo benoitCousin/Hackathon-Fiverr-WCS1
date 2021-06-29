c4770a203b116610a5b80e855daee0395171e6cc
Ludovic RAVINET <ludovic.ravinet@gmail.com>
2021-06-29 15:40:28 +0200

    added challenge create

--- src/Security/AppCustomAuthenticator.php
+++ src/Security/AppCustomAuthenticator.php
@@ -51,7 +51,7 @@ class AppCustomAuthenticator extends AbstractLoginFormAuthenticator
         }
 
         // For example:
-        return new RedirectResponse($this->urlGenerator->generate('dashboard_index'));
+        return new RedirectResponse($this->urlGenerator->generate('profile_show'));
         //throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
     }
 
