edb3685a75c7e1921240efa0bbaad0eb7ff24b21
Ludovic RAVINET <ludovic.ravinet@gmail.com>
2021-06-29 09:42:50 +0200

    added challenge CRUD

---
+++ src/Form/ChallengeType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Challenge;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ChallengeType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title')
+            ->add('description')
+            ->add('status')
+            ->add('validation')
+            ->add('createdAt')
+            ->add('difficultyLevel')
+            ->add('creator')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Challenge::class,
+        ]);
+    }
+}
